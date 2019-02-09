578749eca4e9dacebcafc869bd13dd18d0dfe7ca
Abhay Bharti <abhaybharti@users.noreply.github.com>
2019-02-09 11:28:27 +0530

    9 Feb

---
+++ Java/src/javalearning/chapter2/objectorientedconcepts/InheritanceConcepts.java
@@ -0,0 +1,7 @@
+package javalearning.chapter2.objectorientedconcepts;
+
+/***
+ *  1. if a subclass inherits a member, it is exactly as if the subclass actually declared the member itself
+ */
+public class InheritanceConcepts {
+}
